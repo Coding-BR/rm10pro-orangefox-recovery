8535f0615754a9ac279e10bd6298a3a9d2dc6dbb
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-11-10 21:20:15 +0800

    sm8750: move all odm elf to /odm

--- recovery/root/odm/bin/variant-files-copy.sh
+++ recovery/root/odm/bin/variant-files-copy.sh
@@ -4,7 +4,9 @@
 variant=$(getprop ro.boot.hardware.sku)
 
 cp -rf /vendor/variant/$variant/vendor/* /vendor
+cp -rf /vendor/variant/$variant/odm/* /odm
 chmod -R 755 /vendor/bin/*
+chmod -R 755 /odm/bin/*
 resetprop variant.files_copied "1"
 
 echo "Copy variant files copied for $variant." >/tmp/recovery.log
